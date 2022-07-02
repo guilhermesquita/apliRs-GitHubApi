b8cfef07fcfc198754b4af15ec1d5d5b8b356d93
Guilherme Mesquita <guilhermer_mesquita@hotmail.com>
2022-07-02 10:34:11 -0400

    add components

--- src/components/layout/Index.jsx
+++ src/components/layout/Index.jsx
@@ -1,10 +1,14 @@
 import React from "react";
+import Header from "../header";
+import * as S from "./styled";
 
-function Layout(){
-    return(
-            <header></header>
-        
-    )
-}
+const Layout = ({ children }) => {
+  return (
+    <S.WrapperLayout>
+      <Header />
+      {children}
+    </S.WrapperLayout>
+  );
+};
 
 export default Layout;
